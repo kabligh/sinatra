5512962017c8c53682477f346bd791d68e4da313
K Annabel Bligh <kabligh@gmail.com>
2020-11-09 17:21:22 +0000

    Added shotgun gem

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org" do
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem "shotgun"
+gem "sinatra"
+
+end
--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
+set :session_secret, "super secret"
 
 get '/' do
   "hello!"
 end
 
 get '/secret' do
-  "I hate cats"
-end 
+  "Wouldn't you like to know"
+end
+
+get '/goodbye' do
+  "k thx bai"
+end
+
+get '/hallelujah' do
+  "🙌 🤡 🙏 🥳"
+end
